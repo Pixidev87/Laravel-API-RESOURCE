f8405910db0c088013888bb1e0f05f154d68a371
zoli87webdev <zoli87webdev@gmail.com>
2025-11-19 18:09:53 +0100

    breeze install

--- routes/api.php
+++ routes/api.php
@@ -1,25 +1,8 @@
 <?php
 
-use App\Http\Controllers\Api\V1\PostController as V1PostController;
-// use App\Http\Controllers\Api\V2\PostController as V2PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
-
-// Route::get("/hello", function() {
-//     return response()->json(["message" => "Hello, API!"]);
-// });
-
-
-//API Resource Route for PostController
-Route::prefix('v1')->group(function () {
-    Route::apiResource('posts', V1PostController::class);
 });
-
-// Route::prefix('v2')->group(function () {
-//     Route::apiResource('posts', V2PostController::class);
-// });
-
